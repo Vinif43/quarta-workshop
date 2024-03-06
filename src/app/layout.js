014fd17397cae0e718450db715d796183e8739b4
vinif <vinifmarinhoo@gmail.com>
2024-03-06 14:35:02 -0300

    Atualiza configurações do ESLint e pacotes de dependência***
    ***Atualiza o layout da aplicação e adiciona componente de cabeçalho***
    
    ***Atualiza estilos globais da aplicação

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={[inter.className, "bg-[#1F2937]"].join(" ")}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="flex justify-center py-8">
+      <h1 className="text-4xl text-white text-center font-bold">
+        Rick and Morty Characters
+      </h1>
+    </div>
+  )
+}
